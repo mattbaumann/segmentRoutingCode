6b43df8576ddb2425f9173c4506d1461fc709145
Matt <matthias-baumann@outlook.com>
2019-04-18 20:51:57 +0200

    First implementation base runner

--- src/TestScript.py
+++ src/TestScript.py
@@ -1,42 +1,86 @@
 # import providers, services and models
-from ydk.services import CRUDService
-from ydk.providers import NetconfServiceProvider
-from ydk.models.cisco_ios_xr import Cisco_IOS_XR_shellutil_oper as xr_shellutil_oper
-from datetime import timedelta
-
-from argparse import ArgumentParser
-from urllib.parse import urlparse
 import logging
+import os
+from argparse import ArgumentParser
+from datetime import timedelta
+from pathlib import Path
+from urllib.parse import urlparse
 
-"""
-Create configuration for model Cisco-IOS-XR-segment-routing-ms-cfg.
-usage: nc-create-xr-segment-routing-ms-cfg-10-ydk.py [-h] [-v] device
-positional arguments:
-  device         NETCONF device (ssh://user:password@host:port)
-optional arguments:
-  -h, --help     show this help message and exit
-  -v, --verbose  print debugging messages
-"""
+from ruamel.yaml import YAML
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_shellutil_oper as xr_shellutil_oper
+from ydk.providers import NetconfServiceProvider
+from ydk.services import CRUDService
+
+boolean_dict = {
+    "true": True,
+    "false": False,
+    "yes": True,
+    "no": True,
+    "on": True,
+    "off": False
+}
+config_version = 1
+
+
+def parse_bool(value_string):
+    if isinstance(value_string, bool):
+        return value_string
+    else:
+        return boolean_dict[value_string.lower()]
+
+
+def load_config():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", help="sets the relative path to default config",
+                        default="./Test.yaml")
+    # Parse parameters
+    args = parser.parse_args()
+
+    assert ("config" in args)
+
+    # Parse config
+    script_path = Path(os.path.dirname(os.path.abspath(__file__)))
+    absolute_path = Path(script_path, args.config)
+
+    assert (absolute_path.exists() and absolute_path.is_file())
+
+    yamlParser = YAML(typ='safe')
+    yaml = yamlParser.load(absolute_path)
+
+    assert (yaml['version'] <= config_version)
+
+    return yaml
+
+
+def load_logger():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                   "%(levelname)s - %(message)s"))
+    handler.setFormatter(formatter)
+
+    ydk_logger = logging.getLogger("ydk")
+    ydk_logger.addHandler(handler)
+
+    app_logger = logging.getLogger("APP")
+    app_logger.addHandler(handler)
+
+    verbose = parse_bool(config["config"]["logging"]["verbose"])
+
+    if verbose:
+        ydk_logger.setLevel(logging.INFO)
+        app_logger.setLevel(logging.INFO)
+    else:
+        ydk_logger.setLevel(logging.ERROR)
+        app_logger.setLevel(logging.ERROR)
+    return app_logger
+
 
 if __name__ == "__main__":
-    """Main execution path"""
-    parser = ArgumentParser()
-    parser.add_argument("-v", "--verbose", help="print debugging messages",
-                        action="store_true")
-    parser.add_argument("device",
-                        help="NETCONF device (ssh://user:password@host:port)")
-    args = parser.parse_args()
-    device = urlparse(args.device)
+    config = load_config()
 
-    # log debug messages if verbose argument specified
-    if args.verbose:
-        logger = logging.getLogger("ydk")
-        logger.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
-                                       "%(levelname)s - %(message)s"))
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+    device = urlparse(config["config"]["host"])
+
+    logger = load_logger()
 
     # create NETCONF session
     provider = NetconfServiceProvider(address=device.hostname,
@@ -45,11 +89,7 @@ if __name__ == "__main__":
                                       password=device.password,
                                       protocol=device.scheme)
 
-    # provider = NetconfServiceProvider(address="10.20.0.170",
-    #                                   port=22,
-    #                                   username="system",
-    #                                   password="system",
-    #                                   protocol="ssh")
+    logger.info(f"Connnect to {device}")
 
     # create CRUD service
     crud = CRUDService()
