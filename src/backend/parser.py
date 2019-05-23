1c00802f391d93bce63b6e673caeb19d65aa8505
Matt <matthias-baumann@outlook.com>
2019-05-23 09:17:48 +0200

    Writing parsing and writing functions to router

--- src/backend/parser.py
+++ src/backend/parser.py
@@ -8,24 +8,34 @@ from src.backend.model.policy import Policy
 from src.backend.model.segmentList import SegmentList
 
 
-def parse_policy(sr_config_mapping):
-    policies = []
-    for policy in sr_config_mapping.traffic_engineering.policies.policy:
-        paths = []
-        for preference in policy.candidate_paths.preferences.preference:
-            segments = []
-            for pathinfo in preference.path_infos.path_info:
-                segments.append(SegmentList(preference.path_index.__str__(),
-                                            read_segment_list(sr_config_mapping, pathinfo.segment_list_name)))
-            paths.append(CandidatePath(preference.path_index, segments))
-        policies.append(Policy(policy.policy_name, policy.policy_color_endpoint.color, paths))
-    return policies
+def parse_policy(sr_config_mapping) -> List[Policy]:
+    """Parses policy objects"""
+    segments = parse_segment_list(sr_config_mapping)
+    return list(map(lambda policy:
+                    Policy(policy.policy_name, policy.policy_color_endpoint.color,
+                           parse_candidate_path(policy, segments))
+                    , sr_config_mapping.traffic_engineering.policies.policy))
 
 
-def read_segment_list(sr: sr_config.Sr, segment_name: str):
-    segments: List[Label] = []
-    for segment in sr.traffic_engineering.segments.segment:
-        if segment_name == segment.path_name:
-            for segmentItem in segment.segments.segment:
-                segments.append(Label(segmentItem.mpls_label, segmentItem.segment_type))
-    return segments
+def parse_candidate_path(policy, segments: List[SegmentList]) -> List[CandidatePath]:
+    """Parses a candidate path"""
+    return list(map(
+        lambda preference: CandidatePath(preference.path_index, parse_segment(preference, segments)),
+        policy.candidate_paths.preferences.preference
+    ))
+
+
+def parse_segment(preference, segments: List[SegmentList]) -> List[SegmentList]:
+    """Add segments to policy according to different pathinfo"""
+    result: List[SegmentList] = []
+    for pathinfo in preference.path_infos.path_info:
+        result.extend(filter(lambda segment: segment.name == pathinfo.segment_list_name, segments))
+    return result
+
+
+def parse_segment_list(sr: sr_config.Sr) -> List[SegmentList]:
+    """Parse actual segment list which will be referenced afterwards"""
+    return list(map(
+        lambda segment: SegmentList(segment.path_name, list(map(
+            lambda label: Label(label.mpls_label, label.segment_type), segment.segments.segment))),
+        sr.traffic_engineering.segments.segment))
--- src/backend/writer.py
+++ src/backend/writer.py
@@ -2,14 +2,43 @@ from typing import List
 
 from ydk.models.cisco_ios_xr import Cisco_IOS_XR_segment_routing_ms_cfg as sr_config
 
+from src.backend.model.candidatePath import CandidatePath
+from src.backend.model.policy import Policy
 from src.backend.model.segmentList import SegmentList
 
 
-def write_segments(segments: SegmentList):
+def write_policy(config: sr_config.Sr, policy: Policy):
+    for paths in policy.paths:
+        write_segments(config, paths.hops)  # Write segment list
+
+    color: sr_config.Sr.traffic_engineering.OnDemandColors.OnDemandColor = sr_config.Sr.traffic_engineering.OnDemandColors.OnDemandColor()
+    color.color = policy.color
+    rule: sr_config.Sr.traffic_engineering.policies.policy = sr_config.Sr.traffic_engineering.policies.Policy()
+    rule.policy_name = policy.name
+    rule.policy_color_endpoint = color
+    for path in policy.paths:
+        write_candidate_path(path, config)
+    return rule
+
+
+def write_segments(config: sr_config.Sr, segments: List[SegmentList]):
+    for segment in segments:
+        config.traffic_engineering.segments.segment.append(write_segment(segment))
+
+
+def write_segment(segment: SegmentList):
     result: List[sr_config.Sr.traffic_engineering.segments.Segment] = []
-    for label in segments.labels:
+    for label in segment.labels:
         segment = sr_config.Sr.traffic_engineering.segments.Segment()
         segment.mpls_label = label.label
         segment.segment_type = label.type
         result.append(segment)
     return result
+
+
+def write_candidate_path(path: CandidatePath, config: sr_config.Sr):
+    preference: sr_config.Sr.traffic_engineering.policies.policy.candidate_paths.preferences.Preference()
+    preference.path_index = path.preference
+    info = preference.path_infos.Path_info()
+    info.segment_list_name = path.hops[0].name
+    preference.path_infos.path_info.append(info)
