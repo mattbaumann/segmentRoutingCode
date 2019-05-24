859c04ba851e303d2e669f850c20c89e6c8708c6
Luca <luca.gubler@hispeed.ch>
2019-05-24 09:02:52 +0200

    Write db stuff

--- src/server/routes/controllers.py
+++ src/server/routes/controllers.py
@@ -6,15 +6,16 @@ from pathlib import Path
 from flask import Flask, Blueprint, render_template, request, abort
 
 from src.server.extension.db_connection import db, Policy, CandidatePath, SegmentList
-from src.server.utils.http_status import BAD_REQUEST
+from src.server.utils.http_status import BAD_REQUEST, OK, NO_CONTENT
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 routes = Blueprint('index', __name__, template_folder='templates')
 
+command = OK
+
 raw_json = '[{"name": "P1", "color": 1, "paths": [{"preference": 10, "hops": [{"name": "Plist-1", "labels": [{"label": 16009, "type": "mpls-label"},{"label": 16004, "type": "mpls-label"},{"label": 16005, "type": "mpls-label"}]}]}]}, {"name": "P2", "color": 1, "paths": [{"preference": 10, "hops": [{"name": "Plist-1", "labels": [{"label": 16009, "type": "mpls-label"},{"label": 16004, "type": "mpls-label"},{"label": 16005, "type": "mpls-label"}]}]}]}]'
-nice_json = json.loads(raw_json)
 
 
 @routes.route('/')
@@ -24,24 +25,7 @@ def home():
 
 @routes.route('/about')
 def about():
-    for policy in nice_json:
-        print("POLICY: " + str(policy))
-        insert_into_policy = Policy(str(policy['name']), str(policy['color']), '', '', '')
-        db.session.add(insert_into_policy)
 
-        for candidate_path in policy['paths']:
-            print("CANDIDATE_PATH: " + str(candidate_path))
-            insert_into_candidate_path = CandidatePath(str(candidate_path['preference']))
-            insert_into_policy.candidate_path.append(insert_into_candidate_path)
-            db.session.add(insert_into_candidate_path)
-
-            for hop in candidate_path['hops']:
-                print("HOP: " + str(hop))
-                insert_into_segment_list = SegmentList(str(hop['name']), str(hop['labels']))
-                insert_into_candidate_path.segment_list.append(insert_into_segment_list)
-                db.session.add(insert_into_segment_list)
-
-        db.session.commit()
 
     return render_template('/static/about.html')
 
@@ -91,16 +75,44 @@ def show_segment_list(name, candidate_path_id, segment_list_id):
 
 @routes.route('/update', methods=['POST'])
 def update():
-    json = request.json
+    nice_json = json.loads(raw_json)
+    # nice_json = json.loads(request.json)
     if json is None:
         abort(BAD_REQUEST)
-    # TODO: Add data update handler
+    for policy in nice_json:
+        insert_into_policy = Policy(str(policy['name']), str(policy['color']), '', '', '')
+        db.session.add(insert_into_policy)
+
+        for candidate_path in policy['paths']:
+            insert_into_candidate_path = CandidatePath(str(candidate_path['preference']))
+            insert_into_policy.candidate_path.append(insert_into_candidate_path)
+            db.session.add(insert_into_candidate_path)
+
+            for hop in candidate_path['hops']:
+                insert_into_segment_list = SegmentList(str(hop['name']), str(hop['labels']))
+                insert_into_candidate_path.segment_list.append(insert_into_segment_list)
+                db.session.add(insert_into_segment_list)
+
+        db.session.commit()
+
+
+# 200 OK, wenn config auf router geschrieben wird
+# 204 NO_CONTENT, wenn auf dem router die config auf dem router gelesen
+@routes.route('/command', methods=['POST'])
+def command():
+    # if status_code == OK:
+    #     return 'write config'
+    # elif status_code == NO_CONTENT:
+    #     return 'update config'
+
+    return '', NO_CONTENT
 
 
 @routes.route('/execute', methods=['GET', 'POST'])
 def execute():
     script_path = Path(os.path.dirname(os.path.abspath(__file__)))
     absolute_path = Path(script_path, "../../backend/backend.py").absolute()
-    args = ["/usr/bin/python3", "/home/matt/YDK-Test-Project/src/backend/backend.py", "-s " + request.url_root]
+    args = ["/usr/bin/python3", absolute_path, "-s " + request.url_root]
     subprocess.Popen(args)
     return render_template('update/inprogress.html')
+
